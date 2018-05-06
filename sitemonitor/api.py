65807f29c3c24b30246ad5bb0cd6727b56883039
Ruhshan <ruhshan.ahmed@gmail.com>
2018-05-07 01:13:31 +0600

    fix:wip: enabled permission class allowany

--- sitemonitor/api.py
+++ sitemonitor/api.py
@@ -1,6 +1,9 @@
+from rest_framework.decorators import permission_classes
+
 from .models import Site
 from .serializers import SiteSerializer
 from rest_framework import generics
+from rest_framework.permissions import AllowAny
 
 
 class SiteListAPI(generics.ListAPIView):
@@ -10,7 +13,7 @@ class SiteListAPI(generics.ListAPIView):
         user = self.kwargs['userid']
         return Site.objects.filter(user=int(user), isdeleted=False)
 
-
+@permission_classes((AllowAny, ))
 class SiteUpdateAPI(generics.UpdateAPIView):
     queryset = Site.objects.all()
     serializer_class = SiteSerializer
