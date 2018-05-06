375bb493747cc53e14a849488decb499c4d3a7b5
Ruhshan <ruhshan.ahmed@gmail.com>
2018-05-06 23:45:13 +0600

    feature:complete:  site monitor on/off

--- project/static/js/site_list_scripts.js
+++ project/static/js/site_list_scripts.js
@@ -2,168 +2,178 @@
  * Created by ruhshan on 5/5/18.
  */
 
-    var app = new Vue({
-        el:"#app",
-        data:{
-            showform:false,
-            name:'',
-            url:'',
-            interval:2,
-            objectid:'',
-            allsites:{},
-            refetch:false,
-            accordionindex:'',
-            errors:{
-                name:false,
-                url:false,
-            },
-            errormessages:{},
+var app = new Vue({
+    el: "#app",
+    data: {
+        showform: false,
+        name: '',
+        url: '',
+        interval: 2,
+        objectid: '',
+        allsites: {},
+        refetch: false,
+        accordionindex: '',
+        errors: {
+            name: false,
+            url: false
         },
-        created:function(){
-          uid = window.location.pathname.split('/')[3]
-          datasource = "/sitemonitor/api/v1/list/"+uid;
-          axios.get(datasource).then( function(response) {
-              allsites = response.data
-              //app.$data.allsites = allsites;
+        errormessages: {}
+    },
+    created: function () {
+        uid = window.location.pathname.split('/')[3]
+        datasource = "/sitemonitor/api/v1/list/" + uid;
+        axios.get(datasource).then(function (response) {
+            allsites = response.data
+            //app.$data.allsites = allsites;
+            for (var i = 0; i < allsites.length; i++) {
+                if (allsites[i].isalive) {
+                    allsites[i].image = '/static/green_circle.png'
+                } else {
+                    allsites[i].image = '/static/red_circle.png'
+                }
 
-              for(var i=0;i<allsites.length;i++){
-                  if(allsites[i].isalive){
-                      allsites[i].image = '/static/green_circle.png'
-                  }else{
-                      allsites[i].image = '/static/red_circle.png'
-                  }
+            }
+            app.$data.allsites = allsites;
 
-              }
-              app.$data.allsites = allsites;
+        });
 
-          });
+    },
+
+
+    methods: {
+        togglemonitoring: function (site) {
+            data = {
+                ismonitoring: !site.ismonitoring,
+            };
+            endpoint = "/sitemonitor/api/v1/update/" + site.id;
+            axios.patch(endpoint, data)
+                .then(function (response) {
+                    console.log(response);
+                })
+                .catch(function (error) {
+                    console.log(error);
+                })
+        },
+        update: function (e) {
+            this.reset();
+
+            data = {
+                name: this.name,
+                url: this.url,
+                interval: Math.round(this.interval),
+
+            };
+
+            var endpoint = "/sitemonitor/api/v1/update/" + this.objectid;
+
+            // console.log(JSON.stringify(data));
+            axios.patch(endpoint, data)
+                .then(function (response) {
+                    console.log(response);
+                    app.$data.refetch = true;
+                    setTimeout(function () {
+                        app.$data.showform = false;
+                    }, 500);
+
+                    UIkit.accordion(document.getElementById("listdiv")).toggle(app.$data.accordionindex);
+
+                })
+                .catch(function (error) {
+                    //console.log(error);
+                    app.$data.errormessages = error.response.data;
+                });
+
 
         },
+        edit: function (site, index) {
+            console.log(site.id);
+            this.$data.name = site.name;
+            this.$data.url = site.url;
+            this.$data.interval = site.interval;
+            this.$data.objectid = site.id;
+            this.$data.showform = true;
+            this.accordionindex = index;
 
 
-        methods:{
-            togglemonitoring:function (e) {
-                console.log(e);
-            },
-            update: function (e) {
-                this.reset();
-
+        },
+        del: function (site, index) {
+            UIkit.modal.confirm('Delete ' + site.name + "?").then(function () {
                 data = {
-                    name: this.name,
-                    url: this.url,
-                    interval: Math.round(this.interval),
-
+                    isdeleted: true,
                 };
-                // var csrftoken = Cookies.get('csrftoken');
-                // console.log(csrftoken);
-                var endpoint = "/sitemonitor/api/v1/update/"+this.objectid;
-
-                // console.log(JSON.stringify(data));
-                axios.patch(endpoint,data)
+                endpoint = "/sitemonitor/api/v1/update/" + site.id;
+                axios.patch(endpoint, data)
                     .then(function (response) {
                         console.log(response);
-                        app.$data.refetch=true;
-                        setTimeout(function(){app.$data.showform =false;}, 500);
+                        UIkit.accordion(document.getElementById("listdiv")).toggle(index);
+                        setTimeout(function () {
+                            app.$data.refetch = true;
+                        }, 500);
 
-                        UIkit.accordion(document.getElementById("listdiv")).toggle(app.$data.accordionindex);
 
                     })
                     .catch(function (error) {
-                        //console.log(error);
-                        app.$data.errormessages = error.response.data;
+                        console.log(error);
                     });
 
-
-            },
-            edit:function (site, index) {
-                console.log(site.id);
-                this.$data.name = site.name;
-                this.$data.url = site.url;
-                this.$data.interval = site.interval;
-                this.$data.objectid = site.id;
-                this.$data.showform = true;
-                this.accordionindex = index;
-
-
-
-            },
-            del:function (site, index) {
-                UIkit.modal.confirm('Delete '+site.name+"?").then(function () {
-                    data = {
-                        isdeleted:true,
-                        };
-                    endpoint = "/sitemonitor/api/v1/update/"+site.id;
-                    axios.patch(endpoint,data)
-                        .then(function (response) {
-                            console.log(response);
-                            UIkit.accordion(document.getElementById("listdiv")).toggle(index);
-                            setTimeout(function () {
-                                app.$data.refetch = true;
-
-                            }, 500);
-
-
-                        })
-                        .catch(function (error) {
-                            console.log(error);
-                        })
-
-                }, function () {
-                    console.log('Rejected.')
-                });
-            },
-            cancel: function (e) {
-                setTimeout(function(){app.$data.showform =false;}, 500);
-                UIkit.accordion(document.getElementById("listdiv")).toggle(app.$data.accordionindex);
-
-            },
-            reset:function () {
-                console.log("reset called");
-                this.errors.name=false;
-                this.errors.url=false;
-            },
-
+            }, function () {
+                console.log('Rejected.')
+            });
+        },
+        cancel: function (e) {
+            setTimeout(function () {
+                app.$data.showform = false;
+            }, 500);
+            UIkit.accordion(document.getElementById("listdiv")).toggle(app.$data.accordionindex);
 
         },
-        watch:{
-            interval:function(val){
-                rounded_interval = Math.round(val);
-                this.interval = Math.round(val);
-                document.getElementById('id_interval_rounded').innerHTML = rounded_interval+" Mins.";
-            },
-            errormessages:function (val) {
-                if(val.name){
-                    this.errors.name=true;
-                }
-                if(val.url){
-                    this.errors.url=true;
-                }
+        reset: function () {
+            console.log("reset called");
+            this.errors.name = false;
+            this.errors.url = false;
+        },
 
-            },
-            refetch: function (val) {
-                if (val == true) {
-                    console.log("refetching");
-                    uid = window.location.pathname.split('/')[3]
-                    datasource = "/sitemonitor/api/v1/list/" + uid;
-                    axios.get(datasource).then(function(response) {
-                        allsites = response.data
-                          //app.$data.allsites = allsites;
 
-                          for(var i=0;i<allsites.length;i++){
-                              if(allsites[i].isalive){
-                                  allsites[i].image = '/static/green_circle.png'
-                              }else{
-                                  allsites[i].image = '/static/red_circle.png'
-                              }
+    },
+    watch: {
+        interval: function (val) {
+            rounded_interval = Math.round(val);
+            this.interval = Math.round(val);
+            document.getElementById('id_interval_rounded').innerHTML = rounded_interval + " Mins.";
+        },
+        errormessages: function (val) {
+            if (val.name) {
+                this.errors.name = true;
+            }
+            if (val.url) {
+                this.errors.url = true;
+            }
 
-                          }
-                          app.$data.allsites = allsites;
+        },
+        refetch: function (val) {
+            if (val == true) {
+                console.log("refetching");
+                uid = window.location.pathname.split('/')[3]
+                datasource = "/sitemonitor/api/v1/list/" + uid;
+                axios.get(datasource).then(function (response) {
+                    allsites = response.data
+                    //app.$data.allsites = allsites;
+
+                    for (var i = 0; i < allsites.length; i++) {
+                        if (allsites[i].isalive) {
+                            allsites[i].image = '/static/green_circle.png'
+                        } else {
+                            allsites[i].image = '/static/red_circle.png'
+                        }
+
+                    }
+                    app.$data.allsites = allsites;
                 });
-                    this.refetch=false;
-                }
+                this.refetch = false;
+            }
 
-            },
+        },
 
-        }
+    }
 
-    });
+});
